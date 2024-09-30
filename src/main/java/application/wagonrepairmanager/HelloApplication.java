4cc0b56afdf6a5cf5026d3f6aff8404a0549e7cc
AleexxG <alexanderg.sln@gmail.com>
2024-09-30 11:43:22 +0200

    Style for tab pane added

--- src/main/java/application/wagonrepairmanager/HelloApplication.java
+++ src/main/java/application/wagonrepairmanager/HelloApplication.java
@@ -11,8 +11,11 @@ public class HelloApplication extends Application
     @Override
     public void start(Stage stage) throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/dashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("views/tab-pane.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
         stage.setTitle("Wagon Repair Manager");
         stage.setScene(scene);
         stage.show();
